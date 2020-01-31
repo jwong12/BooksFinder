b077de3d637e0223ad30982f70896bc0229faeae
James Wong <jwong_li@hotmail.com>
2020-01-30 22:56:10 -0800

    render a list of books through API requests from Google Books API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import BookListing from './BookListing';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [],
+      bookInput: '',
+      error: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      bookInput: 'Harry Potter'
+    }, () => {
+      this.getApiRequest(this.state.bookInput);
+    });
+  }
+
+  getApiRequest(keyword) {
+    const newKeyword = keyword.trim().replace(/ /g, '+');
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${newKeyword}&maxResults=40`
+
+    axios.get(url)
+    .then((response) => {
+      this.setState({ books: response.data.items });
+
+      if(response.data.totalItems === 0) {
+        this.setState({ error: `No books found for '${keyword}'.` });
+      }            
+    })
+    .catch((error) => {
+      console.error('There was an error from the api server.');
+    });
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleKeyDown(e) {
+    if(e.keyCode === 13) {
+      this.handleSearchClick();
+    }
+  }
+
+  handleSearchClick() {
+    this.setState({ error: null });
+    this.getApiRequest(this.state.bookInput);
+  }
+
+  render() {
+    const { books, error } = this.state;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Books Collection</h1>
+          <input 
+            name="bookInput"
+            value={this.state.bookInput}
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+            placeholder="Enter title or author"
+          />
+          <button onClick={this.handleSearchClick}>Search</button>
+          {books && (
+            <ul>
+              {books.map((book) => 
+                <li key={book.etag}>
+                  <BookListing 
+                    title={book.volumeInfo.title}
+                    authors={book.volumeInfo.authors}
+                    categories={book.volumeInfo.categories}
+                    description={book.volumeInfo.description}
+                    imgUrl={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail}
+                    volumeUrl={book.volumeInfo.canonicalVolumeLink}
+                  />
+                </li>
+              )}
+            </ul>
+          )}          
+          {error && (
+            <p>{error}</p>
+            )}
+        </header>
+      </div>
+    );
+  }  
 }
 
 export default App;
---
+++ src/BookListing.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class BookListing extends Component {
+    formatAuthors = (authors) => {
+        if(authors && authors.length > 1) {   
+            let authorsConcat = authors[0];
+
+            for(let i = 1; i < authors.length; i++) {
+                if(i === authors.length - 1) {
+                    authorsConcat += ' and ' + authors[i];
+                    return authorsConcat;
+                }
+                authorsConcat += ', ' + authors[i];
+            }
+
+        } else if(authors && authors.length === 1) {
+            return authors;
+        }
+        return "Unknown";
+    }
+
+    formatCategories = (categories) => {
+        if(categories && categories.length >= 1) {
+            let categoriesConcat = categories[0];
+
+            for(let i = 1; i < categories.length; i++) {
+                categoriesConcat += ' | ' + categories[i];
+            }
+            return categoriesConcat;
+        }
+        return "Unknown";
+    }
+
+    render() {
+        return(
+            <div className='book'>
+                <img src={this.props.imgUrl ? this.props.imgUrl : 'unknown.jpg'} alt={this.props.title} />
+                <div className='bookDetails'>
+                    <a href={this.props.volumeUrl} target='_blank' rel='noopener'>{this.props.title}</a>                    
+                    <p>by {this.formatAuthors(this.props.authors)}</p>
+                    <p>Category: {this.formatCategories(this.props.categories)}</p>
+                    <p>Description: {this.props.description}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BookListing;
