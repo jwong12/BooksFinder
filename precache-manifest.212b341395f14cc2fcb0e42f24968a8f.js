87cacf5ed077c8a62a46cbea3a618a4575122d5e
James Wong <jwong_li@hotmail.com>
2020-02-01 15:20:55 -0800

    Updates

---
+++ precache-manifest.212b341395f14cc2fcb0e42f24968a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0318872fb35a50c78fbf3420f714b65a",
+    "url": "/BooksFinder/index.html"
+  },
+  {
+    "revision": "b14d3236bfa54b371178",
+    "url": "/BooksFinder/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "492e93ac9df01b37d6a9",
+    "url": "/BooksFinder/static/css/main.e38d3144.chunk.css"
+  },
+  {
+    "revision": "b14d3236bfa54b371178",
+    "url": "/BooksFinder/static/js/2.69f90f60.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BooksFinder/static/js/2.69f90f60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "492e93ac9df01b37d6a9",
+    "url": "/BooksFinder/static/js/main.05d4c861.chunk.js"
+  },
+  {
+    "revision": "daa08200423bb3119824",
+    "url": "/BooksFinder/static/js/runtime-main.5e99a0e7.js"
+  }
+]);
